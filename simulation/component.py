a543051f8adc3c8326e143ef55abaa6cb2c54fd7
nsouayby-png <nsouayby@gmail.com>
2026-07-09 11:54:36 +0200

    Update component.py

--- simulation/component.py
+++ simulation/component.py
@@ -1,15 +1,14 @@
 import salabim as sim
 
-from .triggerable import Triggerable
+from .ables import Triggerable
 
 
 class Component(sim.Component):
     def request(self, *args, fail_at = None, fail_delay = None, mode = None, urgent = False, request_priority = 0, priority = 0, cap_now = None, oneof = False, called_from = "request"):
         super().request(*args, fail_at=fail_at, fail_delay=fail_delay, mode=mode, urgent=urgent, request_priority=request_priority, priority=priority, cap_now=cap_now, oneof=oneof, called_from=called_from)
 
-        from .resource import Resource  # deferred: resource.py imports Component (circular otherwise)
-
         if not self.failed():
+            from .resource import Resource
             for r, q in args:
                 if isinstance(r, Resource) and r.lifespan < float('inf'):
                     r.shave(q)
@@ -18,7 +17,7 @@ class Component(sim.Component):
 
     def release(self, *args):
         resources = [a[0] if isinstance(a, tuple) else a for a in args] or list(self.claimed_resources())
-        super().release(*args)
         for r in resources:
             if isinstance(r, Triggerable):
                 r.trigger.trigger()
+        super().release(*args)
